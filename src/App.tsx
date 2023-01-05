328e6b1616d12ffe012a62322d47711ce18ed6f4
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-01-05 18:06:44 -0300

    fix: Remove examples

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,9 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 
-import { Button } from './components/Button'
-
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
-
       <GlobalStyle />
     </ThemeProvider>
   )

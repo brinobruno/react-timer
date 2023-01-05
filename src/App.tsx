35328283f21bc5705d44af92fd5d6a1f44faf9ee
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-01-05 18:14:14 -0300

    feat: Add routes with React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }

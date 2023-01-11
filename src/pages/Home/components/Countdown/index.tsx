fc42300196b19882342c7572cff35f58235ef737
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-01-11 19:23:29 -0300

    fix: Update path for receiving context from src/contexts/CyclesContext

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect } from 'react'
 import { differenceInSeconds } from 'date-fns'
 
-import { CyclesContext } from '../..'
+import { CyclesContext } from '../../../../contexts/CyclesContext'
 import { CountdownContainer, CountdownSeparator } from './styles'
 
 export function CountDown() {
